a478655bf181f1780747a962d12642a70e489847
mglowinski <mck00@o2.pl>
2019-05-15 23:08:48 +0200

    add method to calculate fibonacci series recursively

--- src/main/java/com/mglowinski/otp/controller/AppController.java
+++ src/main/java/com/mglowinski/otp/controller/AppController.java
@@ -1,17 +1,26 @@
 package com.mglowinski.otp.controller;
 
+import com.mglowinski.otp.service.AppService;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class AppController {
 
-    @GetMapping("/hello-world")
+    private final AppService appService;
+
+    @Autowired
+    public AppController(AppService appService) {
+        this.appService = appService;
+    }
+
+    @RequestMapping(value = "/hello-world", produces = "application/json")
     public ResponseEntity<Object> hello() throws JSONException {
         JSONObject object = new JSONObject();
         object.put("hello1", "world1");
@@ -21,5 +30,19 @@ public class AppController {
         object.put("hello5", "world5");
         object.put("hello6", "world6");
         return ResponseEntity.ok(object.toString());
+    }
+
+    @RequestMapping(value = "/sum-of-squares", produces = "application/json")
+    public ResponseEntity<Object> getSumOfSquares(@RequestParam(value = "n", required = false) Integer n) {
+        JSONObject object = new JSONObject();
+        int sumOfSquares = appService.getSumOfSquares(n);
+        object.put("sumOfSquares", sumOfSquares);
+        return ResponseEntity.ok(object.toString());
+    }
+
+    @RequestMapping(value = "/fib-series-recursively", produces = "application/json")
+    public ResponseEntity<Object> getFibonacciSeriesRecursively(@RequestParam(value = "n", required = false) Integer n) {
+        int[] sumOfSquares = appService.getFibonacciSeriesRecursively(n);
+        return ResponseEntity.ok(sumOfSquares);
     }
 }
--- src/main/java/com/mglowinski/otp/service/AppService.java
+++ src/main/java/com/mglowinski/otp/service/AppService.java
@@ -5,4 +5,35 @@ import org.springframework.stereotype.Service;
 @Service
 public class AppService {
 
+    public int getSumOfSquares(Integer n) {
+        if (n == null) {
+            n = 10;
+        }
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += (i * i);
+        }
+        return sum;
+    }
+
+    public int[] getFibonacciSeriesRecursively(Integer n) {
+        if (n == null) {
+            n = 10;
+        }
+
+        int[] results = new int[n];
+        for (int i = 0; i < n; i++) {
+            results[i] = fib(i);
+        }
+        return results;
+    }
+
+    private int fib(int n) {
+        if (n > 1) {
+            return fib(n - 1) + fib(n - 2);
+        } else {
+            return n;
+        }
+    }
 }
