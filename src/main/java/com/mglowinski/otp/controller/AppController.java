312e13d02df22f027623cb66351e2a365335a5c0
mglowinski <mck00@o2.pl>
2019-05-15 22:39:55 +0200

    add hello world method

--- src/main/java/com/mglowinski/otp/controller/AppController.java
+++ src/main/java/com/mglowinski/otp/controller/AppController.java
@@ -1,17 +1,25 @@
 package com.mglowinski.otp.controller;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
 
 @RestController
 @RequestMapping("/api")
 public class AppController {
 
-    @GetMapping("/fibonacci-series")
-    public Principal user(Principal principal) {
-        return principal;
+    @GetMapping("/hello-world")
+    public ResponseEntity<Object> hello() throws JSONException {
+        JSONObject object = new JSONObject();
+        object.put("hello1", "world1");
+        object.put("hello2", "world2");
+        object.put("hello3", "world3");
+        object.put("hello4", "world4");
+        object.put("hello5", "world5");
+        object.put("hello6", "world6");
+        return ResponseEntity.ok(object.toString());
     }
 }
